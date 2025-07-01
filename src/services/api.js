6aba5a985c00ba8996a34f30ee312c44624351cd
Sherman97 <grmanrjas@gmail.com>
2025-07-01 10:08:04 -0500

    fix, change enpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Ajusta si es necesario
+  baseURL: 'https://api-hotel-01nz.onrender.com/api', // Ajusta si es necesario
   headers: {
     'Content-Type': 'application/json',
   },
